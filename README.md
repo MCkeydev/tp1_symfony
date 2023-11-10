8edf3d91747cc4f9181fdbd34a2e1d3a4dd23cbb
Matthieu Courréjou <mattcourrejou@gmail.com>
2023-11-10 15:30:14 +0100

    Fin de TP2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# tp1_symfony
+# symfonyIUT
---
+++ src/Controller/BlogController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * La route suivante accepte deux paramètres : "id" et "title", qui sont respectivement un int et une string.
+     */
+    #[Route('/blog/{id}/{title}', name: 'app_blog', methods: ['GET'], requirements: ['name' => '[a-zA-Z]{5,50}', 'id' => '[0-9]{2,6}'])]
+    public function index($id, $title): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'id' => $id,
+            'title' => $title,
+        ]);
+    }
+
+    /**
+     * Route GET du contrôleur afin de tester le principe de routage.
+     */
+    #[Route('/', name: 'app_hello_world', methods: ['GET'])]
+    public function helloWorld(): Response
+    {
+        // Renvoie au client un header d'ordre 1
+        return new Response('<h1>Hello world !</h1>');
+    }
+}
